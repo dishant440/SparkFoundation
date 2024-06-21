92169c5ab0cdd585423a12c580c28dc07838bcd8
Dishant <dishantd442442@gmail.com>
2024-06-21 13:11:18 +0530

    added route for getting transfer history

--- backend/routes/Customer.js
+++ backend/routes/Customer.js
@@ -3,6 +3,50 @@ const router = express.Router();
 const { Customer, TransferTable } = require("../database/db");
 const mongoose = require("mongoose");
 
+
+
+router.get('/history', async (req, res) => {
+  try {
+    const transferHistory = await TransferTable.aggregate([
+      {
+        $lookup: {
+          from: 'customers', // Name of the Customer collection in MongoDB
+          localField: 'fromId', // Field from TransferTable
+          foreignField: '_id', // Field from Customer collection
+          as: 'fromCustomer' // The new field to add to the transferHistory with the matched customer documents
+        }
+      },
+      {
+        $lookup: {
+          from: 'customers', // Name of the Customer collection in MongoDB
+          localField: 'toId', // Field from TransferTable
+          foreignField: '_id', // Field from Customer collection
+          as: 'toCustomer' // The new field to add to the transferHistory with the matched customer documents
+        }
+      },
+      {
+        $project: {
+          _id: 1,
+          amount: 1,
+          date: 1,
+          fromId: 1,
+          toId: 1,
+          'fromCustomer.name': 1, // Include only the customer name in the response
+          'toCustomer.name': 1 // Include only the customer name in the response
+        }
+      }
+    ]);
+
+    return res.json({
+      transferHistory
+    });
+  } catch (error) {
+    return res.json({
+      error: error.message
+    });
+  }
+});
+
 router.post("/transfer", async (req, res) => {
   let session;
 
@@ -72,7 +116,6 @@ router.post("/transfer", async (req, res) => {
   }
 });
 
-// Get all customers route
 router.get("/allCustomer", async (req, res) => {
   try {
     const users = await Customer.find({});
