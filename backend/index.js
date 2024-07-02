04a4638b69a2e88a3fad1857904b5630832198f6
Dishant <dishantd442442@gmail.com>
2024-07-02 20:17:16 +0530

    added script

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const {ConnectToDB} = require('./database/db')
 const rootRouter = require('./routes/index')
 const app = express();
 const cors = require('cors')
-
+const port = process.env.PORT || 8080;
 const mongoDBUri = process.env.DATABASE_URL;
 console.log(mongoDBUri);
 
@@ -14,4 +14,4 @@ app.use("/api",rootRouter);
 ConnectToDB(mongoDBUri);
 
 
-app.listen(8080,()=>console.log('listening on port 8080'));
+app.listen(port,()=>console.log(`listening on port ${port} `));
