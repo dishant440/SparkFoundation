cf668966654c2efa319480facef92674c3679e68
Dishant <dishantd442442@gmail.com>
2024-07-03 19:37:56 +0530

    made some changes transaction route

--- backend/routes/Customer.js
+++ backend/routes/Customer.js
@@ -1,7 +1,7 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const { Customer, TransferTable } = require("../database/db");
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
+const { Customer, TransferTable } = require('../database/db'); // Adjust the import according to your setup
 
 router.post("/transfer", async (req, res) => {
   let session;
@@ -10,48 +10,36 @@ router.post("/transfer", async (req, res) => {
     session = await mongoose.startSession();
     session.startTransaction();
 
-    const senderEmail = req.body.sender;
+    const { sender: senderEmail, receiver: receiverEmail, amount } = req.body;
 
-    const receiverEmail = req.body.receiver;
-    const amount = req.body.amount;
-
-    const SenderAccount = await Customer.findOne({
-      email: senderEmail,
-    }).session(session);
-
+    const SenderAccount = await Customer.findOne({ email: senderEmail }).session(session);
     if (!SenderAccount) {
       await session.abortTransaction();
-      return res.status(400).json({
-        message: "Invalid sender account",
-      });
-    }
-    if (SenderAccount.balance < amount) {
-      await session.abortTransaction();
-      return res.status(400).json({
-        message:"Insufficient balance"
-      })
-      
+      return res.status(400).json({ message: "Invalid sender account" });
     }
 
-    const ReceiverAccount = await Customer.findOne({
-      email: receiverEmail,
-    }).session(session);
+    if (SenderAccount.balance < amount) {
+      await session.abortTransaction();
+      return res.status(400).json({ message: "Insufficient balance" });
+    }
+
+    const ReceiverAccount = await Customer.findOne({ email: receiverEmail }).session(session);
     if (!ReceiverAccount) {
       await session.abortTransaction();
-      return res.status(400).json({
-        message: "Invalid receiver account",
-      });
+      return res.status(400).json({ message: "Invalid receiver account" });
     }
 
     await Customer.updateOne(
       { email: senderEmail },
-      { $inc: { balance: -amount } }
-    ).session(session);
+      { $inc: { balance: -amount } },
+      { session }
+    );
 
     await Customer.updateOne(
       { email: receiverEmail },
-      { $inc: { balance: amount } }
-    ).session(session);
+      { $inc: { balance: amount } },
+      { session }
+    );
 
     const transfer = new TransferTable({
       fromId: SenderAccount._id,
@@ -62,91 +50,17 @@ router.post("/transfer", async (req, res) => {
     await transfer.save({ session });
 
     await session.commitTransaction();
+    session.endSession();
 
-    res.json({
-      message: "Transfer successful",
-    });
+    res.json({ message: "Transfer successful" });
   } catch (error) {
     if (session) {
       await session.abortTransaction();
-    }
-    console.error(error);
-    res.status(500).json({ message: "Internal Server Error" });
-  } finally {
-    if (session) {
       session.endSession();
     }
-  }
-});
-
-router.get("/allCustomer", async (req, res) => {
-  try {
-    const users = await Customer.find({});
-
-    res.json({
-      users,
-    });
-  } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Internal Server Error" });
   }
 });
 
-router.get("/history", async (req, res) => {
-  try {
-    const transferHistory = await TransferTable.aggregate([
-      {
-        $lookup: {
-          from: "customers",
-          localField: "fromId",
-          foreignField: "_id",
-          as: "fromCustomer",
-        },
-      },
-      {
-        $lookup: {
-          from: "customers",
-          localField: "toId",
-          foreignField: "_id",
-          as: "toCustomer",
-        },
-      },
-      {
-        $project: {
-          _id: 1,
-          amount: 1,
-          date: 1,
-          fromId: 1,
-          toId: 1,
-          "fromCustomer.name": 1,
-          "toCustomer.name": 1,
-        },
-      },
-    ]);
-
-    return res.json({
-      transferHistory,
-    });
-  } catch (error) {
-    return res.json({
-      error: error.message,
-    });
-  }
-});
-
-router.get("/:id", async (req, res) => {
-  const Id = req.params.id;
-
-  try {
-    const user = await Customer.findById({ _id: Id });
-    res.json({
-      user,
-    });
-    return;
-  } catch (error) {
-    res.json({
-      error: "invalid user",
-    });
-  }
-});
 module.exports = router;
